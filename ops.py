e83f740e604bcc40f41bbf07bfddbb449e0bdd1d
andreistirb <andreistirb@yahoo.com>
2018-03-22 09:32:10 +0200

    we do not need reshape in the transposed convolutional layer because its shape is already the needed one

--- ops.py
+++ ops.py
@@ -80,7 +80,7 @@ def deconv2d(input_, output_shape,
                 strides=[1, d_h, d_w, 1])
 
     biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
-    deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
+    deconv = tf.nn.bias_add(deconv, biases)
 
     if with_w:
       return deconv, w, biases
